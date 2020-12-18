ce80cf26f9f34a5d7636cf6c67a7bb042c96447e
MPCBBishop <guyi2000@yeah.net>
2020-12-18 17:25:26 +0800

    Add course Form

--- course/forms.py
+++ course/forms.py
@@ -10,7 +10,17 @@ class CourseForm(forms.Form):
     course_id = forms.CharField(label="课程号",
                                 max_length=128,
                                 widget=forms.TextInput(attrs={'class':'form-control'}))
-    #school = forms.ModelChoiceField(label="课程号",
-    #                           max_length=128,
-    #                            widget=forms.TextInput(attrs={'class':'form-control'}))
-    
+    school = forms.ModelChoiceField(queryset=School.objects.all(),
+                                    widget=forms.Select(attrs={'class':'form-control'}))
+    department = forms.CharField(label="院系",
+                                 max_length=128,
+                                 widget=forms.TextInput(attrs={'class':'form-control'}))
+    teacher = forms.CharField(label="开课教师", 
+                              max_length=128,
+                              widget=forms.TextInput(attrs={'class':'form-control'}))
+    credit = forms.IntegerField(label="学分", 
+                                widget=forms.NumberInput(attrs={'class':'form-control'}))
+    hour = forms.IntegerField(label="学时", 
+                              widget=forms.NumberInput(attrs={'class':'form-control'}))
+    pre_course = forms.CharField(label="先修要求",
+                                 widget=forms.Textarea(attrs={'class':'form-control'}))
--- course/views.py
+++ course/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from .forms import CourseForm
-
+from search.forms import SearchForm, SmallSearchForm
 # Create your views here.
 def addcourse(request):
     course_form = CourseForm()
+    search_form = SearchForm()
+    small_search_form = SmallSearchForm()
     return render(request, 'course/addcourse.html', locals())
 
 def courseinfo(request, course_id):
+    search_form = SearchForm()
+    small_search_form = SmallSearchForm()
     return render(request, 'course/course.html')
 
 def changecourseinfo(request):
+    search_form = SearchForm()
+    small_search_form = SmallSearchForm()
     return render(request, 'course/changecourseinfo.html')
